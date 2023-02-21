9e3c836b23f425132c859309518290d16d26a5e5
Ben Borgers <borgersbenjamin@gmail.com>
2023-02-21 13:44:33 -0500

    first commit

---
+++ api/html.ts
@@ -0,0 +1,9 @@
+export const config = {
+  runtime: "edge",
+};
+
+export default (req: Request) => {
+  return new Response(`The time is ${new Date().toISOString()}`, {
+    headers: { "content-type": "text/html" },
+  });
+};
