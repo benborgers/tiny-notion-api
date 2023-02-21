b3a63d38989ce3dae1cfef928c7efeca670f6390
Ben Borgers <borgersbenjamin@gmail.com>
2023-02-21 13:48:11 -0500

    Stylistic change

--- api/html.ts
+++ api/html.ts
@@ -1,6 +1,4 @@
-export const config = {
-  runtime: "edge",
-};
+export const config = { runtime: "edge" };
 
 export default (req: Request) => {
   return new Response(`The time is ${new Date().toISOString()}`, {
